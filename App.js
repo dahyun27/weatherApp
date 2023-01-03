7df9645d896f07144f713c7af1a09947a17802f2
하다현 <dahyeon@hadahyeon-ui-MacBookAir.local>
2023-01-03 17:17:24 +0900

    change Text color

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi, I'm Dahyeon</Text>
+      <Text style={styles.Text}>Hi, I'm Dahyeon</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  Text: {
+    color: "blue",
   },
 });
